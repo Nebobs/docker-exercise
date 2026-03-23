2f77e135a4916fb015c9a5bb2d9270225d89c038
Nevo Benzvi <nevo.benzvi44@gmail.com>
2026-03-23 20:14:15 +0200

    renamed c

---
+++ Dockerfile.c
@@ -0,0 +1,18 @@
+FROM python:3.12
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y openssh-server 
+
+RUN python -m venv /opt/venv
+ENV PATH="/opt/venv/bin:$PATH"
+
+COPY . /app
+
+RUN pip install -r requirements.txt
+
+RUN mkdir -p /var/run/sshd && useradd -m -s /bin/bash rotem && echo "rotem:81" | chpasswd
+
+EXPOSE 22
+
+CMD ["/usr/sbin/sshd", "-D"]
